7743985c7ecfb191fdc1c6698f093d368518c5b0
mdrhmn48 <mdrhmn48@gmail.com>
2023-04-11 23:30:47 -0400

    done

---
+++ week2/app.py
@@ -0,0 +1,8 @@
+#importing libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model  import LogisticRegression
+import streamlit as st
+
+st.header("Alhamdulilah")
