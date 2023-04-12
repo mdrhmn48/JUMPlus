9036fa93345832c0ea5240744bcac330160e7a4f
mdrhmn48 <mdrhmn48@gmail.com>
2023-04-12 13:08:07 -0400

    one chart done

--- week2/app.py
+++ week2/app.py
@@ -4,29 +4,59 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model  import LogisticRegression
 import streamlit as st
-import os
+import pandas_bokeh
+from bokeh.plotting import figure, show
+file = "https://raw.githubusercontent.com/mdrhmn48/JUMPlus/main/week2/charcters_stats.csv"
+def load_data():
+   df = pd.read_csv(file)
+
+   return df
+  
+df = load_data()
+st.title(":red[Welcome to] :green[Marvel Data Analysis] 🤖👿🦸‍♂️⚒️:bar_chart:")
+st.markdown("Marvel Data Analysis Dashboard, let's you explore the relationship between each super powers and villians")
+# st.set_page_config(layout="wide")
+scatter_fig = df.plot_bokeh.scatter(x="Intelligence", y="Strength", category = None,
+                            xlabel = "Intelligence", ylabel="Strength", title = "Superheroes Superpowers",
+                            fontsize_title = 25, fontsize_label=12,
+                            figsize=(750,500), show_figure=False
+                            )
+st.bokeh_chart(scatter_fig)
+
+# def create_bar(col):
+#     plt.figure(figsize = (10,6))
+#     temp_df = df.groupby("Alignment")[col].mean().round(decimals=2).reset_index()
+#     ax = sns.barplot(
+#         data = temp_df,
+#         x = "Alignment",
+#         y = col,
+#         palette = ["#941A00", "#439429", "#1E38AD"]
+#     )
+#     plt.title(f"{col} with respect to Alignment", fontsize =  15, weight = "bold");
+#     for i in ax.containers:
+#         ax.bar_label(i,)
+#     plt.show()
+
+# cols = ['Intelligence', 'Strength', 'Speed', 'Durability', 'Power', 'Combat']
+# for col in cols:
+#     bar_plot = create_bar(col)
+#     st.bar_chart(bar_plot)
 
 
+# p = figure(x_range="Intelligence", height=350, title="Fruit Counts",
+#            toolbar_location=None, tools="")
 
-st.header("Welcome to Marvel Dataset")
-#file = "charcters_stats.csv"
-# file = open(file)
-# file.read()
-# DATE_COLUMN = 'date/time'
-# DATA_URL = (file)
-# @st.cache_data
-def read_data(file):
-    data = pd.read_csv(file) #path folder of the data file
-    return data
+# p.vbar(x="Strength", top=counts, width=0.9)
 
+# p.xgrid.grid_line_color = None
+# p.y_range.start = 0
 
-
-st.title("Hello world! hi how are you")
-
-uploaded_file = st.file_uploader("charcters_stats.csv")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write("dataframe")
-# data = read_data(file)
-# st.title('Super Heroes Vs Super Villians')
-
+bar_plot = df.plot_bokeh(
+    kind='bar',
+    x='Total',
+    y=['Intelligence', 'Strength', 'Durability'],
+    xlabel='Category',
+    ylabel='Annual Sales',
+    title='Annual Sales by Category'
+  )
+st.bar_chart(bar_plot)
